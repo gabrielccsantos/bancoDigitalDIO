ba242c6b8344cee580cce0fa60cd803e1459949a
gabriel santos <gabrielcarneirocsantos@gmail.com>
2024-05-15 14:38:23 -0300

    Aula Encapsulamento

--- src/entities/ContaCorrente.java
+++ src/entities/ContaCorrente.java
@@ -5,6 +5,18 @@ public class ContaCorrente {
     private int numero;
     private double saldo;
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
     public void sacar(){
 
     }
